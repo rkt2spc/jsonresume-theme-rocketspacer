fbfe8a785d223beacfcafa0fe83cee5f69837e34
Tuan Nguyen <nmtuan.dev@gmail.com>
2020-05-28 03:39:24 +0800

    Trim code

--- index.js
+++ index.js
@@ -3,28 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const ejs = require('ejs');
 
-// Constants
-const NODE_ENV = process.env.NODE_ENV || 'development';
-
-// Helpers
 const helpers = require('./helpers');
 
-// Production render method
-const productionRender = (() => {
-  // Pre-building template
-  const filename = path.join(__dirname, 'views/resume.ejs');
-  const template = ejs.compile(fs.readFileSync(filename, 'utf8'), {
-    filename : filename,
-    context  : { ...helpers },
-  });
-  const css = fs.readFileSync(path.join(__dirname, 'assets/stylesheets/style.css'), 'utf8');
-
-  // Return function that use the pre-built template
-  return (resume) => template({ css, resume });
-})();
-
 // Development render method
-const developmentRender = (resume) => {
+exports.render = (resume) => {
   const filename = path.join(__dirname, 'views/resume.ejs');
   const template = ejs.compile(fs.readFileSync(filename, 'utf8'), {
     filename : filename,
@@ -33,9 +15,4 @@ const developmentRender = (resume) => {
   const css = fs.readFileSync(path.join(__dirname, 'assets/stylesheets/style.css'), 'utf8');
 
   return template({ css, resume });
-};
-
-// Exports
-module.exports = {
-  render: NODE_ENV !== 'development' ? productionRender : developmentRender,
 };
